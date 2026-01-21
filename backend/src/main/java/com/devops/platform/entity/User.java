96916b1291d9ca2ff51e807333d425b5aec4a7f9
Bogdan Chudopalov <tchudopalov.bogdan@yandex.ru>
2026-01-21 12:53:34 +0300

    add User.java entity

---
+++ backend/src/main/java/com/devops/platform/entity/User.java
@@ -0,0 +1,25 @@
+package com.devops.platform.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class User implements UserDetails{
+    
+}
