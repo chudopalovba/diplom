55595edd91c9bcf7eb659e1d1296044dea3d5408
Bogdan Chudopalov <tchudopalov.bogdan@yandex.ru>
2026-01-21 12:55:43 +0300

    edit User.java

--- backend/src/main/java/com/devops/platform/entity/User.java
+++ backend/src/main/java/com/devops/platform/entity/User.java
@@ -22,4 +22,59 @@ import java.util.List;
 
 public class User implements UserDetails{
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "gitlab_user_id")
+    private Long gitlabUserId;
+
+    @Column(name = "gitlab_username")
+    private String gitlabUsername;
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<Project> projects = new ArrayList<>();
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updateAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public boolean isAccountNoneExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked(){
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled(){
+        return true;
+    }
 }
