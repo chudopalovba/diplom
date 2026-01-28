ff8a4f38b96491e51a0bf2285a8c063bacd57c56
Bogdan Chudopalov <tchudopalov.bogdan@yandex.ru>
2026-01-28 17:19:29 +0300

    add Project entity

---
+++ backend/src/main/java/com/devops/platform/entity/Project.java
@@ -0,0 +1,12 @@
+package com.devops.platform.entity;
+
+import com.devops.entity.enums.ProjectStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
