0568f69acc96be901459090b63c4bf2ae8832fea
Bogdan Chudopalov <tchudopalov.bogdan@yandex.ru>
2026-06-23 22:36:44 +0300

    rm target

--- backend/target/classes/templates/backend/python/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import JsonResponse
-
-
-def health(request):
-    return JsonResponse({'status': 'UP', 'project': '{{PROJECT_NAME}}'})
-
-
-def hello(request):
-    return JsonResponse({'message': 'Hello from {{PROJECT_NAME}}!'})
